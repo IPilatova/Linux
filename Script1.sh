cbfdb3d510d226f71afd8a0ee04cf2e1e973656d
IPilatova <irina.pilatova@gmail.com>
2021-09-20 23:27:35 +0300

    added hw

---
+++ Script1.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#1. Написать скрипт который выполнит автоматически пункты 3, 4, 5, 6, 7, 8, 13
+cd Lesson_1_Git_bash/
+mkdir HW_1 folder{1..3}
+cd folder1
+touch file{1..3}.txt file{4,5}.json
+mkdir game_process levels user_manual
+ls -la
+mv file4.json file5.json ../folder2
